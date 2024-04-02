f93945a9f16e8e02f87b03d2d987d12a1bbe4be4
4trastos <93524280+4trastos@users.noreply.github.com>
2024-04-02 14:17:56 +0200

    Update checker.c

--- philo/src/checker.c
+++ philo/src/checker.c
@@ -39,19 +39,22 @@ int	ft_checker(char **argv)
 	return (1);
 }
 
-int	ft_checkerphilo(char **argv)
+int	ft_checkerphilo(char **argv, int argc)
 {
 	int numb;
 
+	if (argc == 6)
+	{
+		if ((numb = ft_atoi(argv[5]) == 0))
+			return (0);
+	}
 	if ((numb = ft_atoi(argv[1]) == 0))
 		return (0);
-	else if ((numb = ft_atoi(argv[2]) == 0))
+	else if ((numb = ft_atoi(argv[2]) == 0) || (numb = ft_atoi(argv[2]) < 60))
 		return (0);
-	else if ((numb = ft_atoi(argv[3]) == 0))
+	else if ((numb = ft_atoi(argv[3]) == 0) || (numb = ft_atoi(argv[3])) < 60)
 		return (0);
-	else if ((numb = ft_atoi(argv[4]) == 0))
-		return (0);
-	else if ((numb = ft_atoi(argv[5]) == 0))
+	else if ((numb = ft_atoi(argv[4]) == 0) || (numb = ft_atoi(argv[4])) < 60)
 		return (0);
 	else
 		return (1);
