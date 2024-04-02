e6b2bdab8683709bc7a67ee4250ff24bc3c07713
4trastos <93524280+4trastos@users.noreply.github.com>
2024-04-02 17:18:26 +0200

    Update philo.h

--- philo/incl/philo.h
+++ philo/incl/philo.h
@@ -42,13 +42,13 @@ typedef pthread_mutex_t t_mtx;
 typedef struct s_fork
 {
 	t_mtx			fork;
-	int				fork_id;
+	int			fork_id;
 }	t_fork;
 
 typedef struct s_philo
 {
-	int				id;
-	int				fork;
+	int			id;
+	int			fork;
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
@@ -63,8 +63,8 @@ typedef struct s_philo
 
 typedef struct s_philo_test
 {
-	int				id;
-	int				fork;
+	int			id;
+	int			fork;
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
@@ -74,7 +74,7 @@ typedef struct s_philo_test
 
 //*** INIT ****
 
-int					main(int argc, char **argv);
+int				main(int argc, char **argv);
 
 //*** ERRORS ****
 
@@ -87,9 +87,9 @@ void				error_msg(char *str);
 
 //*** CHECKER ****
 
-int					ft_checker(char **argv);
-int					ft_is_numer(char c);
-int					ft_checkerphilo(char **argv, int argc);
+int				ft_checker(char **argv);
+int				ft_is_numer(char c);
+int				ft_checkerphilo(char **argv, int argc);
 
 //*** PHILO ****
 
@@ -99,7 +99,7 @@ void				ft_argcount(t_philo *a, int arc, char **argv);
 
 void				ft_stackphilo(t_philo **a, int arc, char **argv);
 t_philo				*ft_createnode(int id, int fork,  t_philo_test *aux);
-t_philo_test		*ft_new_test(int arc, char **argv);
+t_philo_test			*ft_new_test(int arc, char **argv);
 void				ft_stacknode(t_philo **a, t_philo *new);
 
 //*** AUXILIARS ****
