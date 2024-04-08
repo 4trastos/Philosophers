4126ebe603aaea4e8f6c440f2685c3818c163a09
4trastos <93524280+4trastos@users.noreply.github.com>
2024-04-08 12:26:39 +0200

    Update philo.h

--- philo/incl/philo.h
+++ philo/incl/philo.h
@@ -21,6 +21,7 @@
 # include <stdbool.h>
 # include <pthread.h>
 # include <sys/time.h>
+# include <errno.h>
 
 /*
  * PHILO
@@ -48,79 +49,92 @@ typedef enum e_opcode
 	DETACH,
 }			t_opcode;
 
-typedef pthread_mutex_t	t_mtx;
+typedef pthread_mutex_t	t_mutex;
+typedef struct s_table t_table;
+
+pthread_mutex_t	mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct s_fork
 {
-	t_mtx			fork;
-	int			fork_id;
+	t_mutex			fork;
+	int				fork_id;
 }	t_fork;
 
 typedef struct s_philo
 {
-	int			id;
-	int			fork;
-	long			time_to_die;
-	long			time_to_eat;
-	long			time_to_sleep;
-	long			philo_number;
-	long			number_of_times;
+	int				id;
+	bool			full;
 	long			meals_counter;
 	long			last_meal_time;
-	bool			full;
-	struct s_philo	*next;
-	struct s_philo	*prev;
+	pthread_t		thread_id;
+	t_fork			*first_fork;
+	t_fork			*second_fork;
+	t_mutex			philo_mutex;
+	t_table			*table;
 }	t_philo;
 
-typedef struct s_philo_test
+struct s_table
 {
-	int			id;
-	int			fork;
 	long			time_to_die;
 	long			time_to_eat;
 	long			time_to_sleep;
-	long			number_of_times;
+	long			nbr_limit_meals;
 	long			philo_number;
 	long			start_simulation;
 	bool			end_simulation;
-}	t_philo_test;
+	bool			all_threads_ready;
+	long			threads_running_nbr;
+	pthread_t		monitor;
+	t_fork			*forks;
+	t_philo			*philos;
+	t_mutex			table_mutex;
+	t_mutex			write_mutex;
+};	
 
 //*** INIT ****
 
-int				main(int argc, char **argv);
+int					main(int argc, char **argv);
 
 //*** ERRORS ****
 
 void				ft_error_msg(char *str, t_philo *a);
-void				free_matrix(t_philo **a);
-void				ft_freelist(t_philo **a);
 void				error_exit(const char *error);
-void				ft_parse(char **argv, int argc);
+void				parse_input(char **argv, int argc);
 void				error_msg(char *str);
 
 //*** CHECKER ****
 
-int				ft_checker(char **argv);
-int				ft_is_numer(char c);
-int				ft_checkerphilo(char **argv, int argc);
+int					ft_checker(char **argv);
+int					ft_is_numer(char c);
+int					ft_checkerphilo(char **argv, int argc);
 bool				is_space(char c);
 
 //*** PHILO ****
 
 void				ft_argcount(t_philo *a, int arc, char **argv);
-void				ft_data_init(t_philo_test	*test, t_philo **a);
-void				ft_dinner_init(t_philo **a);
+void				data_init(t_table *table, int argc, char **argv);
+void				eat_start(t_table *table);
+void				assign_forks(t_philo *philo, t_fork *forks, int id);
+void				philo_init(t_table *table);
 
 //*** NODES ****
 
-void				ft_stackphilo(t_philo_test	*test, t_philo **a, int arc, char **argv);
-t_philo				*ft_createnode(int id, int fork,  t_philo_test *aux);
-t_philo_test			*ft_new_test(int arc, char **argv);
+t_table				*ft_new_table(int arc, char **argv);
 void				ft_stacknode(t_philo **a, t_philo *new);
 
 //*** AUXILIARS ****
 
 long				ft_atoi(char *str);
-void				printlist(t_philo **a);
+
+//*** MUTEX ****
+
+void				*philo_routine(t_table *table);
+
+//*** MUTEX ****
+
+void				*safe_malloc(size_t bytes);
+void				safe_mutex_handle(t_mutex *mutex, t_opcode opcode);
+void				safe_thread_handle(pthread_t *thread, void *(*foo)(void *),
+						void *data, t_opcode opcode);
 
 #endif
