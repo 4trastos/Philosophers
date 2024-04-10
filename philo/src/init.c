c3d3123c89c30a0edb95f72b44a5168a81dac43b
4trastos <93524280+4trastos@users.noreply.github.com>
2024-04-10 20:03:23 +0200

    Update init.c

--- philo/src/init.c
+++ philo/src/init.c
@@ -17,48 +17,50 @@ void	*unique_philo(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-	wait_all_threads(philo->table);
-	set_long(&philo->philo_mutex, &philo->last_meal_time, gettime(MILLISECOND));
-	increase_long(&philo->table->table_mutex,
-		&philo->table->threads_running_nbr);
-	write_status(TAKE_FIRST_FORK, philo, DEBUG_MODE);
-	while (!simulation_finished(philo->table))
-		precise_usleep(200, philo->table);
+	write(1,"TAKE FIRST FORK\n", 16);
+
 	return (NULL);
 }
-
-void	*philo_routine(t_table *table)
+void	eat(t_philo *philo)
 {
-	int	i;
+	ft_mutex(&philo->first_fork, LOCK);
+	ft_mutex(&philo->second_fork, LOCK);
+	philo->meals_counter++;
+	ft_mutex(&philo->first_fork, UNLOCK);
+	ft_mutex(&philo->second_fork, UNLOCK);
+}
 
-	i = -1;
-	if (table->nbr_limit_meals == 0)
-		return ;
-	else if (table->philo_number == 1)
-		{
-			ft_threads(&table->philos[0].thread_id, unique_philo, &table->philos[0], CREATE);
-		}
+void	*eat_simulation(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	set_long(&philo->philo_mutex, &philo->last_meal_time, gettime(MILLISECOND));
+	while (!simulation_finished(philo->table))
+	{
+		if (get_bool(&philo->philo_mutex, &philo->full))
+			break;
+		eat(philo);
+	}
+	return (NULL);
 }
 
 void	eat_start(t_table *table)
 {
-	pthread_t	*philos;
-	long		dead;
-	int			i;
+	int	i;
 
-	i = 1;
-	philos = malloc(sizeof(pthread_t) * table->philo_number);
-	if (!philos)
+	i = 0;
+	if (table->nbr_limit_meals == 0)
 		return ;
-	dead = table->time_to_die;
-	while (dead > 0)
+	else if (table->philo_number == 1)
+			ft_threads(&table->philos[0].thread_id, unique_philo, &table->philos[0], CREATE);
+	else
 	{
 		while (i < table->philo_number)
 		{
-			if (0 != pthread_create(&philos[i], NULL, philo_routine(table), &i))
-				error_exit("🚨 Error creating thread 🚨\n");
+			ft_threads(&table->philos[i].thread_id, eat_simulation, &table->philos[i], CREATE);
 			i++;
 		}
-		i = 1;
+		write(1,"SALE DEL HILO\n", 14);
 	}
 }
